e44f5790a9347acc7944c73be06a78772712243a
elisamunoz <elisamunozes@gmail.com>
2023-04-12 22:44:12 +0200

    first commit

---
+++ src/ui/components/tile/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Wrapper } from "./Tyle.styles";
+
+const Tile = () => {
+    return (
+        <Wrapper>I am a tile</Wrapper>
+    )
+}
+
+export default Tile;
